0bfd62a037031112b2e6dfd01636bedb0fe236ff
overlookmotel <theoverlookmotel@gmail.com>
2019-04-27 15:12:21 +0100

    Tests: Clearer unhandled rejection errors

--- test/utils/index.js
+++ test/utils/index.js
@@ -5,8 +5,13 @@
 
 'use strict';
 
-// Throw any unhandled promise rejections
-process.on('unhandledRejection', (err) => {
-	console.log('Unhandled rejection'); // eslint-disable-line no-console
-	throw err;
+/*
+ * Throw any unhandled promise rejections.
+ * For easier debugging of where an unhandled rejection is coming from,
+ * if rejected Promise has a `._name` property, it is included in the error message.
+ */
+process.on('unhandledRejection', (err, promise) => {
+	let ctr = promise.constructor.name;
+	if (promise._name) ctr += ` ${promise._name}`;
+	throw new Error(`Unhandled rejection: ${(err || {}).message} (${ctr})`);
 });
